f82029286c53c97b4d434008a29b1c5e8fa850f4
Anton Kaliuzhnyi <m.a.r.kalyuzhny@gmail.com>
2024-10-24 18:34:53 +0300

    Day 05 part 2

--- Sources/Day05.swift
+++ Sources/Day05.swift
@@ -10,7 +10,9 @@ struct Day05: AdventDay {
   
   var data: String
   
-  private var seeds: [Int] = []
+  private var seedsPart1: [Int] = []
+  
+  private var seedsPart2: [Int] = []
   
   private var seedToSoilRanges: [CorrespondanceRange] = []
   
@@ -28,7 +30,6 @@ struct Day05: AdventDay {
   
   private mutating func fetchData() {
     let lines = data.components(separatedBy: "\n")
-    fetchSeeds(lines: lines)
     seedToSoilRanges = fetchRanges(lines: lines, title: "seed-to-soil")
     soilToFertilizerRanges = fetchRanges(lines: lines, title: "soil-to-fertilizer")
     fertilizerToWaterRanges = fetchRanges(lines: lines, title: "fertilizer-to-water")
@@ -38,16 +39,29 @@ struct Day05: AdventDay {
     humidityToLocationRanges = fetchRanges(lines: lines, title: "humidity-to-location")
   }
   
-  private mutating func fetchSeeds(lines: [String]) {
-    if let firstLine = lines.first {
-      let keyValue = firstLine.split(separator: ": ")
-      if keyValue.count > 1 {
-        seeds = keyValue[1].split(separator: " ").compactMap { Int($0) }
-      }
+  private func fetchSeedsPart1() -> [Range<Int>] {
+    let lines = data.split(separator: "\n")
+    guard let firstLine = lines.first else { return [] }
+    let keyValue = firstLine.split(separator: ": ")
+    guard keyValue.count > 1 else { return [] }
+    return keyValue[1].split(separator: " ").compactMap {
+      guard let number = Int($0) else { return nil }
+      return Range<Int>(number...number)
     }
   }
   
-  private mutating func fetchRanges(lines: [String], title: String) -> [CorrespondanceRange] {
+  private func fetchSeedsPart2() -> [Range<Int>] {
+    var seeds: [Range<Int>] = []
+    let values = fetchSeedsPart1()
+    for (i, v) in values.enumerated() {
+      guard i % 2 == 0 else { continue }
+      let range = (v.startIndex)..<(v.startIndex + values[i + 1].startIndex)
+      seeds.append(range)
+    }
+    return seeds
+  }
+  
+  private func fetchRanges(lines: [String], title: String) -> [CorrespondanceRange] {
     var ranges: [CorrespondanceRange] = []
     guard let seedToSoilLineIndex = lines.firstIndex(where: { $0.hasPrefix(title) }) else { return [] }
     for i in (seedToSoilLineIndex + 1)..<lines.count {
@@ -69,6 +83,7 @@ struct Day05: AdventDay {
          source < range.sourceRangeStart + range.rangeLength {
         let offset = source - range.sourceRangeStart
         destination = range.destinationRangeStart + offset
+        break
       }
     }
     if let destination {
@@ -78,25 +93,87 @@ struct Day05: AdventDay {
     }
   }
   
-  func part1() -> Any {
+  private func subtractRanges(inputRange: Range<Int>, intersectionRange: Range<Int>) -> Set<Range<Int>> {
+    guard inputRange.overlaps(intersectionRange) else {
+      return [inputRange]
+    }
+    var resultRanges: Set<Range<Int>> = []
+    if intersectionRange.lowerBound > inputRange.lowerBound {
+      resultRanges.insert(inputRange.lowerBound..<intersectionRange.lowerBound)
+    }
+    if intersectionRange.upperBound < inputRange.upperBound {
+      resultRanges.insert(intersectionRange.upperBound..<inputRange.upperBound)
+    }
+    return resultRanges
+  }
+  
+  private func intersection(inputRanges: Set<Range<Int>>, correspondanceRanges: [CorrespondanceRange]) -> Set<Range<Int>> {
+    var outputRanges: Set<Range<Int>> = []
+    var additionalInputRanges: Set<Range<Int>> = []
+    for inputRange in inputRanges {
+      var hasIntersection = false
+      for correspondanceRange in correspondanceRanges {
+        let sourceRange = correspondanceRange.sourceRangeStart..<(correspondanceRange.sourceRangeStart + correspondanceRange.rangeLength)
+        
+        let intersection = inputRange.clamped(to: sourceRange)
+        if !intersection.isEmpty {
+          hasIntersection = true
+          let offset = correspondanceRange.destinationRangeStart - correspondanceRange.sourceRangeStart
+          outputRanges.insert((intersection.lowerBound + offset)..<(intersection.upperBound + offset))
+          
+          let subtractedInputRanges = subtractRanges(inputRange: inputRange, intersectionRange: intersection)
+          additionalInputRanges.formUnion(subtractedInputRanges)
+        }
+      }
+      if !hasIntersection {
+        outputRanges.insert(inputRange)
+      }
+    }
+    
+    for inputRange in additionalInputRanges {
+        var hasIntersection = false
+        for correspondanceRange in correspondanceRanges {
+          let sourceRange = correspondanceRange.sourceRangeStart..<(correspondanceRange.sourceRangeStart + correspondanceRange.rangeLength)
+          
+          let intersection = inputRange.clamped(to: sourceRange)
+          if !intersection.isEmpty {
+            hasIntersection = true
+            let offset = correspondanceRange.destinationRangeStart - correspondanceRange.sourceRangeStart
+            outputRanges.insert((intersection.lowerBound + offset)..<(intersection.upperBound + offset))
+          }
+        }
+        if !hasIntersection {
+          outputRanges.insert(inputRange)
+        }
+    }
+    return outputRanges
+  }
+  
+  private func minLocation(seeds: [Range<Int>]) -> Int {
+    let soilRanges = intersection(inputRanges: Set(seeds), correspondanceRanges: seedToSoilRanges)
+    let fertilizerRanges = intersection(inputRanges: soilRanges, correspondanceRanges: soilToFertilizerRanges)
+    let waterRanges = intersection(inputRanges: fertilizerRanges, correspondanceRanges: fertilizerToWaterRanges)
+    let lightRanges = intersection(inputRanges: waterRanges, correspondanceRanges: waterToLightRanges)
+    let temperatureRanges = intersection(inputRanges: lightRanges, correspondanceRanges: lightToTemperatureRanges)
+    let humidityRanges = intersection(inputRanges: temperatureRanges, correspondanceRanges: temperatureToHumidityRanges)
+    let locationRanges = intersection(inputRanges: humidityRanges, correspondanceRanges: humidityToLocationRanges)
     var minLocation: Int = .max
-    for seed in seeds {
-      let soil = destination(source: seed, ranges: seedToSoilRanges)
-      let fertilizer = destination(source: soil, ranges: soilToFertilizerRanges)
-      let water = destination(source: fertilizer, ranges: fertilizerToWaterRanges)
-      let light = destination(source: water, ranges: waterToLightRanges)
-      let temperature = destination(source: light, ranges: lightToTemperatureRanges)
-      let humidity = destination(source: temperature, ranges: temperatureToHumidityRanges)
-      let location = destination(source: humidity, ranges: humidityToLocationRanges)
-      if location < minLocation {
-        minLocation = location
+    for locationRange in locationRanges {
+      if locationRange.lowerBound < minLocation {
+        minLocation = locationRange.lowerBound
       }
     }
     return minLocation
   }
   
+  func part1() -> Any {
+    print("Part 1 started")
+    return minLocation(seeds: fetchSeedsPart1())
+  }
+  
   func part2() -> Any {
-    return 0
+    print("Part 2 started")
+    return minLocation(seeds: fetchSeedsPart2())
   }
   
 }
